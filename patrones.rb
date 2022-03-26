67f7bf198553784d84a8a7da01f4a48779678b07
FtobarV64 <ftobarv@gmail.com>
2022-03-26 13:14:29 -0300

    finished: letra_x

--- patrones.rb
+++ patrones.rb
@@ -1,10 +1,3 @@
-# d. Método letra_x(n)
-# n = 5
-# * *
-# * *
-# *
-# * *
-# * *
 # e. Método numero_cero(n)
 # n = 5
 # *****
@@ -95,3 +88,33 @@ def letra_z(n)
 end
 
 letra_z(4)
+
+#---------------------------------------------------------------------
+# d. Método letra_x(n)
+# n = 5
+# *   *
+#  * *
+#   *
+#  * *
+# *   *
+
+# 1 0 0 0 1 i == 0 j == 0 || i == 0 j == 4
+# 0 1 0 1 0 i == 1 j == 1 || i == 1 j == 3
+# 0 0 1 0 0 i == 2 j == 2 || i == 2 j == 2
+# 0 1 0 1 0 i == 3 j == 3 || i == 3 j == 1
+# 1 0 0 0 1 i == 4 j == 4 || i == 4 j == 0
+
+def letra_x(n)
+    n.times do |i|
+        n.times do |j|
+            if i==j || j == (n-1) - i
+                print "* "
+            else
+                print "  "
+            end
+        end
+        print "\n"
+    end
+end
+
+letra_x(11)
